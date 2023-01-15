7d4dfc4071121e986a54b96988dc66b441ba980c
keshu4 <68559994+keshu4@users.noreply.github.com>
2023-01-15 19:17:01 +0530

    build folder added

---
+++ build/precache-manifest.78d315b8a2a1fca7fff113d7122e0c4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62d1dddac2961f6b5b6775c0ed8d646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "040bacdc871eac6a94c0",
+    "url": "/static/css/main.946cfce7.chunk.css"
+  },
+  {
+    "revision": "7789d6b626a072ee2526",
+    "url": "/static/js/2.9582b8ca.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.9582b8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040bacdc871eac6a94c0",
+    "url": "/static/js/main.82a4750e.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  },
+  {
+    "revision": "c4158b64297f39523a5a9b6494ba3842",
+    "url": "/static/media/chat.c4158b64.png"
+  },
+  {
+    "revision": "1b943d724cb2d7c49f888f750ce3a479",
+    "url": "/static/media/google.1b943d72.png"
+  }
+]);
